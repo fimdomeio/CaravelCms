cdf4cf352c4fe5fac616f48ba29b9c077d858a9f
Alexandre <alexandre@fimdomeio.com>
2014-10-29 19:34:59 +0000

    adding Boats Route

--- fimdomeio/caravel/src/routes.php
+++ fimdomeio/caravel/src/routes.php
@@ -1,8 +1,10 @@
 <?php
-
 //namespace Fimdomeio\Caravel;
 
 Route::get('/admin', function()
 {
 	return View::make('caravel::admin.dashboard');
 });
+
+Route::resource('boats', 'Fimdomeio\Caravel\BoatsController');
+
