f1eadc2896b6fa6a79d40f3e59c9aa83021f84b5
Alexandre <alexandre@fimdomeio.com>
2014-10-29 19:34:05 +0000

    Beggining to create example content

---
+++ fimdomeio/caravel/src/controllers/BoatsController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Fimdomeio\Caravel;
+
+
+class BoatsController extends \BaseController {
+
+	public function __construct(){
+		$this->title = 'Boats';
+		//parent::__construct();
+	}
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		$fields   = ['name', 'build_date'];
+		$contents = Boat::all(); 
+		return \View::make('caravel::admin.index')->with('title', $this->title)->with('fields', $fields)->with('contents', $contents);
+	}
+
+
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return Response
+	 */
+	public function create()
+	{
+		return \View::make('caravel::admin.boats.create')->with('title', $this->title);
+	}
+
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @return Response
+	 */
+	public function store()
+	{
+		//
+	}
+
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($id)
+	{
+		//
+	}
+
+
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function edit($id)
+	{
+		//
+	}
+
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function update($id)
+	{
+		//
+	}
+
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy($id)
+	{
+		//
+	}
+
+
+}
---
+++ fimdomeio/caravel/src/models/Boat.php
@@ -0,0 +1,15 @@
+<?php
+namespace Fimdomeio\Caravel;
+
+class Boat extends \Eloquent {
+
+
+	/**
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'boats';
+
+
+}
