1db3588c574bd5299c0be31bea320db81422ae01
Alexandre <alexandre@fimdomeio.com>
2014-11-05 16:33:31 +0000

    Adding not saved message

--- fimdomeio/caravel/src/views/admin/boats/create.blade.php
+++ fimdomeio/caravel/src/views/admin/boats/create.blade.php
@@ -2,14 +2,21 @@
 
 
 @section('content')
-<h1>Add {{$title}}</h1>
+<h1>Add {{str_singular($title)}}</h1>
+@if($errors->has())
+<div class="alert alert-danger col-md-12" role="alert">
+<b>Could not save {{str_singular($title)}}</b><br/>
+Some fields need to be reviewed
+</div>
+
+@endif
 {{ Form::open(array('action' => '\Fimdomeio\Caravel\BoatsController@store')) }}
 
 
 <fieldset class="col-md-4">
 	<legend>Basic Info</legend>
 	{{Form::textField('name', 'Name')}}
-	{{Form::textField('build_date', 'Date')}}
+	{{Form::textField('build_date', 'Build Date')}}
 	{{Form::textareaField('description', 'description')}}
 
 	{{Form::radioButtonsField('published', [
