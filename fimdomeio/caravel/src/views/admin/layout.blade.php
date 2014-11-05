23e2f5615ab1db6981c3c31e572146192d38b43b
Alexandre <alexandre@fimdomeio.com>
2014-11-05 16:34:21 +0000

    Adding first theme draft

--- fimdomeio/caravel/src/views/admin/layout.blade.php
+++ fimdomeio/caravel/src/views/admin/layout.blade.php
@@ -4,6 +4,7 @@
 	<meta charset='UTF-8' />
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>@yield("title") | Caravel</title>
+	<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,200,200italic,300,300italic,400italic,600,600italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
 	<link href='/packages/fimdomeio/caravel/css/admin/style.css' rel='stylesheet' />
 </head>
 <body>
