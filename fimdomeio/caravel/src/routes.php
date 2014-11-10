db4aab86c802fd3c69744ac1e473dde02f22810c
Jose Santos <jsworkbox@gmail.com>
2014-11-10 16:05:22 +0000

    adding auth routes.

--- fimdomeio/caravel/src/routes.php
+++ fimdomeio/caravel/src/routes.php
@@ -1,16 +1,61 @@
 <?php
+
 //namespace Fimdomeio\Caravel;
 
-Route::get('admin', function()
-{
-	return View::make('caravel::admin.dashboard');
-});
+Route::get('/', function()
+	{
+		$title = 'Home';
+		return View::make('caravel::home')
+			->with('title', $title);
+	});
 
-Route::resource('boats', '\Fimdomeio\Caravel\BoatsController');
+Route::get('admin', array(
+	'before' => 'auth',
+	function()
+		{
+			$title = 'Dashboard';
+			return View::make('caravel::admin.dashboard')
+				->with('title', $title);
+		}
+	));
 
-/*** Auth controllers ***/
-Route::get('login', '\Fimdomeio\Caravel\AuthController@showlogin');
-Route::post('login', '\Fimdomeio\Caravel\AuthController@dologin');
-Route::get('logout', '\Fimdomeio\Caravel\AuthController@logout');
-Route::get('register', '\Fimdomeio\Caravel\AuthController@register');
-Route::post('register', '\Fimdomeio\Caravel\AuthController@store');
+Route::resource('boats', '\Fimdomeio\Caravel\BoatsController', array(
+		'before' => 'auth'
+	));
+
+
+	/**
+	 * Auth controllers
+	 *
+	 * Only logout Route is available to authenticated users,
+	 * the rest is guest only for security reasons.
+	 */
+Route::get('login', array(
+	'before' => 'guest',
+	'as' => 'auth.login.show',
+	'uses' => 'AuthController@showLogin'
+	));
+
+Route::post('login', array(
+	'before' => 'guest',
+	'as' => 'auth.login.do',
+	'uses' => 'AuthController@doLogin'
+	));
+
+Route::get('logout', array(
+	'before' => 'auth',
+	'as' => 'auth.logout.do',
+	'uses' => 'AuthController@logout'
+	));
+
+Route::get('register', array(
+	'before' => 'guest',
+	'as' => 'auth.register.show',
+	'uses' => 'AuthController@showRegister'
+	));
+
+Route::post('register', array(
+	'before' => 'guest',
+	'as' => 'auth.register.do',
+	'uses' => 'AuthController@doRegister'
+	));
