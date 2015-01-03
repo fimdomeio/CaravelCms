03d7a7657fa0049559591daaade49807a1de8114
fimdomeio <alexandre@fimdomeio.com>
2015-01-03 18:44:19 +0000

    remove index php file

--- src/index.php
+++
@@ -1,4 +0,0 @@
-<?php 
-echo '<p><a href="http://www.fimdomeio.com">Fimdomeio</a> base <a href="https://github.com/fimdomeio/fimdomeioDefaultVagrant">Vagrant project</a>.</p>
-<p>The file you\'re looking at lives on the \'src\' dir. Go crazy</p>';
-?>
