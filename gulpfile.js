33e03bf1105f30cc2f0871937cc6a868ddae8c34
fimdomeio <alexandre@fimdomeio.com>
2015-01-05 17:01:46 +0000

    automatically copying bower components to their final location

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,15 @@ var less = require('gulp-less');
 var path = require('path');
 var browserSync = require('browser-sync');
 var reload      = browserSync.reload;
+var shell = require('gulp-shell')
 
 var lessFiles = ['src/public/css/style.less', 'src/public/css/variables.css'];
 var htmlFiles = 'src/public/**/**.html';
 var jsFiles = 'src/public/**/**.js';
 
-gulp.task('default', ['browser-sync','less', 'html', 'js'], function() {
+gulp.task('default', ['bowercopy', 'browser-sync','less', 'html', 'js'], function() {
+
+
   gulp.watch(lessFiles, ['less']);
   gulp.watch(htmlFiles, ['html']);
   gulp.watch(jsFiles, ['js']);
@@ -26,6 +29,22 @@ gulp.task('html', function(){
   gulp.src(htmlFiles)
   .pipe(reload({stream:true})); //Browser Sync
 });
+
+gulp.task('bowercopy', ['bowerUpdate'],function(){
+  gulp.src([
+    'src/bower_components/angular/angular.js',
+    'src/bower_components/angular-bootstrap/ui-bootstrap.js',
+    'src/bower_components/angular-loading-bar/build/loading-bar.js',
+    'src/bower_components/lodash/dist/lodash.js',
+    'src/bower_components/restangular/dist/restangular']
+    )
+  .pipe(gulp.dest('src/public/js/'))
+});
+
+gulp.task('bowerUpdate', shell.task([
+  'cd /vagrant/src',
+  '/usr/local/bin/bower update'
+]));
 
 gulp.task('js', function(){
   gulp.src(jsFiles)
