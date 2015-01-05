8c74e465a5d22139c5c9db2947ffa42255d6302e
fimdomeio <alexandre@fimdomeio.com>
2015-01-05 16:40:37 +0000

    Return Caravel issues on github

---
+++ src/app/controllers/CaravelController.php
@@ -0,0 +1,14 @@
+<?php
+
+class CaravelController extends Controller {
+	
+	public function issues(){
+		ini_set('user_agent','Caravel'); 
+		$issues = file_get_contents('https://api.github.com/repos/fimdomeio/CaravelCms/issues?state=open');
+		if(!$issues){
+			return ['data' => null, 'error' => 'nothing was returned'];
+		}
+		$issues = json_decode($issues);
+		return $issues;
+	}
+}
