b7bc14deaa1bca3bf3175abfcc1ffe81f2b2de13
fimdomeio <alexandre@fimdomeio.com>
2015-02-24 11:04:37 +0000

    adding views for sucessful registration as well as email verification

---
+++ src/resources/views/auth/successful-registration.blade.php
@@ -0,0 +1,16 @@
+@extends('app')
+
+@section('content')
+<div>
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-md-6 col-md-offset-3 text-center">
+					<h1>Registration Successful</h1>
+					<div class="alert alert-warning mmt" role="alert">
+						Please check your e-mail to activate your account
+					</div>
+					<a class="btn btn-primary" href="/">home</a>
+				</div>
+	</div>
+</div>
+@endsection
